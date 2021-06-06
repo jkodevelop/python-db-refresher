eba73598a9abee981c855cdf8f15f480bb25fec4
jasonko-home <jasonko@sb2.ca>
2021-06-06 15:59:59 -0400

    step 3: added POST method example 1. added POST method handling   - includes documentation function api.model() for POST usage from flask_restx   - added "request" from flask to process the post body
    - updated code style based on PEP8 code style guideline
    - updated source.md

--- routes/basic.py
+++ routes/basic.py
@@ -1,8 +1,24 @@
-from flask_restx import Namespace, Resource
+from flask_restx import Namespace, Resource, fields
+from flask import Flask, request
 
 api = Namespace('basic', description='basic examples')
 
-@api.route('/hello')
+basic_post =  api.model('basic_post', {
+  'id': fields.String(required=True,description='id'),
+  'active': fields.Boolean(required=True,default=True,description='active: true or false'),
+  'text': fields.String(default='some text',description='text')
+})
+
+@api.route('/example')
 class HelloWorld(Resource):
   def get(self):
     return {'hello': 'world'}
+
+  @api.expect(basic_post, validate=True)
+  def post(self):
+    post_param = request.get_json()
+    return {
+      'status': 'success',
+      'res': post_param
+    }
+    api.abort(404, 'invalid: could not complete api call')
--- source.md
+++ source.md
@@ -4,3 +4,6 @@ https://realpython.com/python-print/
 
 ### package namespace and __init__
 https://stackoverflow.com/questions/37139786/is-init-py-not-required-for-packages-in-python-3-3
+
+### python styleguide PEP8
+https://realpython.com/python-pep8/
