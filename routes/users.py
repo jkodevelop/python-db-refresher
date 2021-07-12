8e88d760b6b5026f74331cc20cf7a7f0a43fd579
jasonko-home <jasonko@sb2.ca>
2021-07-12 00:22:54 -0400

    [Neo4j] added example for Create, Delete and Update 1. api POST -> create user node using cypher 2. api DELETE -> delete user node by uid attribute 3. api PUT -> update user node by uid attribute
    - some code clean up

--- routes/users.py
+++ routes/users.py
@@ -3,24 +3,33 @@ from flask import Flask, request
 from neo4j import GraphDatabase
 
 from util.config import config
-from util.helpers import get_uid
-from util.cypher.users import latest, getById
+from util.cypher.users import latest, get_by_uid, create, delete_by_uid, update_by_uid
 
 # driver = GraphDatabase.driver('neo4j://localhost:7687', auth=('neo4j', 'password'), encrypted=True, trust=False)
 # session.write_transaction(add_friend, "Arthur", "Merlin")
 
 def serialize_user(u):
   return {
+    'uid': u['uid'],
     'first_name': u['firstname'],
     'last_name': u['lastname'],
+    'email': u['email'],
+    'active': u['active']
   }
 
 api = Namespace('users', description='Users management')
 
+user_node =  api.model('basic_post', {
+  'email': fields.String(required=True, description='email of user'),
+  'firstname': fields.String(required=True, default='',description='first name'),
+  'lastname': fields.String(required=True, default='',description='last name'),
+  'active': fields.Boolean(default=False,description='active: true or false')
+})
+
 @api.route('/latest', doc={ 
   'description': 'get latest user'
 })
-class HelloWorld(Resource):
+class Latest(Resource):
   def get(self):
     driver = GraphDatabase.driver(config['neo4j']['DB_CONN_STR'], auth=(config['neo4j']['DB_USER'], config['neo4j']['DB_PASS']), encrypted=False)
     with driver.session(database=config['neo4j']['DB_DBNAME']) as session:
@@ -29,15 +38,64 @@ class HelloWorld(Resource):
     # print(records)
     return [serialize_user(record['u']) for record in records]
 
-@api.route('/get/<id>',doc={ 
-  'description': 'get user by ID'
+@api.route('/get/<int:uid>',doc={ 
+  'description': 'get user by UID'
 })
-@api.doc(params={'id': 'An ID'})
-class ParamExample(Resource):
-  def get(self, id):
-    driver = GraphDatabase.driver('neo4j://localhost:7687', auth=(config['neo4j']['DB_USER'], config['neo4j']['DB_PASS']), encrypted=False)
+@api.doc(params={'uid': 'An ID'})
+class GetUID(Resource):
+  def get(self, uid):
+    driver = GraphDatabase.driver(config['neo4j']['DB_CONN_STR'], auth=(config['neo4j']['DB_USER'], config['neo4j']['DB_PASS']), encrypted=False)
     with driver.session(database=config['neo4j']['DB_DBNAME']) as session:
-      records = session.read_transaction(getById,id)
+      records = session.read_transaction(get_by_uid,uid)
+    driver.close()
+    print(records)
+    return [serialize_user(record['u']) for record in records]
+
+@api.route('/create', doc={ 
+  'description': 'create a User node'
+})
+class CreateUser(Resource):
+  @api.expect(user_node, validate=True)
+  def post(self):
+    post_param = request.get_json()
+    driver = GraphDatabase.driver(config['neo4j']['DB_CONN_STR'], auth=(config['neo4j']['DB_USER'], config['neo4j']['DB_PASS']), encrypted=False)
+    with driver.session(database=config['neo4j']['DB_DBNAME']) as session:
+      records = session.write_transaction(create,post_param)
+    driver.close()
+    print(records)
+    return serialize_user(records[0]['u'])
+    api.abort(404, 'invalid: could not complete api call')
+
+@api.route('/delete/<int:uid>', doc={ 
+  'description': 'delete a User node by uid'
+})
+@api.doc(params={'uid': 'An ID'})
+class DeleteUser(Resource):
+  def delete(self, uid):
+    driver = GraphDatabase.driver(config['neo4j']['DB_CONN_STR'], auth=(config['neo4j']['DB_USER'], config['neo4j']['DB_PASS']), encrypted=False)
+    with driver.session(database=config['neo4j']['DB_DBNAME']) as session:
+      records = session.write_transaction(delete_by_uid,uid)
     driver.close()
     # print(records)
-    return [serialize_user(record['u']) for record in records]
+    # print(records.single())
+    return { 'status':'success' }
+    api.abort(404, 'invalid: could not complete api call')
+
+@api.route('/update/<int:uid>', doc={ 
+  'description': 'update a user node attributes'
+})
+@api.doc(params={'uid': 'An ID'})
+class UpdateUser(Resource):
+  @api.expect(user_node, validate=True)
+  def put(self, uid):
+    post_param = request.get_json()
+    driver = GraphDatabase.driver(config['neo4j']['DB_CONN_STR'], auth=(config['neo4j']['DB_USER'], config['neo4j']['DB_PASS']), encrypted=False)
+    with driver.session(database=config['neo4j']['DB_DBNAME']) as session:
+      records = session.write_transaction(update_by_uid,uid,post_param)
+    driver.close()
+    print(records)
+    if len(records) == 0:
+      return { 'status':'no records updated' }
+    else:
+      return serialize_user(records[0]['u'])
+    api.abort(404, 'invalid: could not complete api call')
--- util/cypher/users.py
+++ util/cypher/users.py
@@ -1,3 +1,5 @@
+from util.helpers import get_uid
+
 def latest(tx):
   records = list()
   cypher = 'MATCH (u:User) RETURN u ORDER BY id(u) DESC LIMIT 1'
@@ -7,6 +9,21 @@ def latest(tx):
     print(record['u'])
   return records
 
-def getById(tx,id):
-  cypher = 'MATCH (u) WHERE id(u)=toInteger($id) RETURN u'
-  return list(tx.run(cypher, id=id))
+def get_by_uid(tx,uid):
+  cypher = 'MATCH (u:User{uid:toInteger($uid)}) RETURN u'
+  return list(tx.run(cypher, uid=uid))
+
+def create(tx,user):
+  cypher = 'CREATE (u:User {uid:toInteger($uid),email:$email,firstname:$firstname,lastname:$lastname,active:$active}) RETURN u'
+  return list(tx.run(cypher, 
+    uid=get_uid(), email=user['email'],firstname=user['firstname'],lastname=user['lastname'],active=user['active']))
+  # return record.single()[0]
+
+def delete_by_uid(tx,uid):
+  cypher = 'MATCH (u:User{uid:toInteger($uid)}) DELETE u'
+  return tx.run(cypher, uid=uid)
+
+def update_by_uid(tx,uid,user):
+  cypher = 'MATCH (u:User{uid:toInteger($uid)}) SET u.email=$email,u.firstname=$firstname,u.lastname=$lastname,u.active=$active RETURN u'
+  return list(tx.run(cypher, 
+    uid=uid, email=user['email'],firstname=user['firstname'],lastname=user['lastname'],active=user['active']))
